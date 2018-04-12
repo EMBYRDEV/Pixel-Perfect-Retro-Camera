b2b2fc01f6385c47c51632f37e7f9bc0f245ea48
Hannah Crawford <hannah@wolv.co>
2018-04-12 11:06:52 +0100

    Added camera control to example scene.

---
+++ Assets/Wolv Interactive/Pixel Perfect Retro Camera/Scenes/Example/MoveCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+	private float spd = 3f;
+
+	void Update() {
+		Vector2 v = new Vector2(Input.GetAxis("Horizontal") * spd, Input.GetAxis("Vertical") * spd);
+
+		transform.Translate(v * Time.deltaTime);
+	}
+}
--- Assets/Wolv Interactive/Pixel Perfect Retro Camera/Scenes/Example/SineMove.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SineMove : MonoBehaviour {
-	void Update () {
-		transform.position = new Vector3(Mathf.Sin(Time.time) * 3f, Mathf.Sin(Time.time * 0.5f) * 1f, -10f);
-	}
-}
